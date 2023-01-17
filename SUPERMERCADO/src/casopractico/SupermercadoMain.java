a07bd555776b8552b38294b069148a24553aad14
Gonzalo Herrera <juan.herrera@live.u-tad.com>
2023-01-17 12:51:56 +0100

    Prueba

--- SUPERMERCADO/src/casopractico/SupermercadoMain.java
+++ SUPERMERCADO/src/casopractico/SupermercadoMain.java
@@ -3,7 +3,9 @@ package casopractico;
 public class SupermercadoMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+		System.out.println("hola cabezon");
 
 	}
 
