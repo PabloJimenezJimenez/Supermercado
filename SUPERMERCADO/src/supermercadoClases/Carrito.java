e98523f45abbd9c76713626be790b4296a66a90c
Pablo Jimenez <Pablo.jimenez2@live.u-tad.com>
2023-03-16 14:32:52 +0100

    commit carrito

--- SUPERMERCADO/src/supermercadoClases/Carrito.java
+++ SUPERMERCADO/src/supermercadoClases/Carrito.java
@@ -2,11 +2,13 @@ package supermercadoClases;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 
 
-public class Carrito {
 
+public class Carrito{
+	static Scanner sc= new Scanner(System.in);
 	private HashMap<Productos,Integer> carrito;
 	
 	public Carrito() {
@@ -33,9 +35,27 @@ public class Carrito {
 	public void eleminarProductoCarrito(Productos a) {
 		
 		if(carrito.containsKey(a)) {
-			int sumarstock= a.getStock()+carrito.get(a);
-			a.setStock(sumarstock);
-			carrito.remove(a);
+			menuEliminar();
+			int num=sc.nextInt();
+			if(num==1) {
+				System.out.println("Productos en el carrito: "+carrito.get(a));
+				System.out.println("Productos a eliminar:");
+				int cantidad=sc.nextInt();
+				if(cantidad>carrito.get(a)) {
+					System.out.println("Cantidad superior a la contenida en el carrito");
+				}else {
+					a.setStock(a.getStock()+cantidad);
+					carrito.put(a,carrito.get(a)-cantidad);
+					System.out.println("Cantidad modificada");
+					System.out.println(a.getNombre()+" "+carrito.get(a));
+				}
+			}
+			if(num==2) {
+				int sumarstock= a.getStock()+carrito.get(a);
+				a.setStock(sumarstock);
+				carrito.remove(a);
+			}
+			
 		}else System.out.println("Producto no encontrado en el carrito");
 		
 		
@@ -43,10 +63,10 @@ public class Carrito {
 	}
 	
 	public void mostrarCarrito() {
-
 		if(carrito.size()==0) {
 			System.out.println("La cesta está vacia");
 		}
+		
 		for (Map.Entry<Productos, Integer> elem : carrito.entrySet()) {
 			System.out.printf("%-9s %duds %9.2f€/uds\n",elem.getKey().getNombre(),elem.getValue(),elem.getKey().getPrecio());
 		}
@@ -61,7 +81,6 @@ public class Carrito {
 		
 	}
 	
-
 	public double totalCompra() {
 		double total=0;
 		//Recorro el diccionario
@@ -70,5 +89,10 @@ public class Carrito {
 			total+=elem.getValue()*elem.getKey().getPrecio();
 		}
 		return total;
+	}
+	private void menuEliminar() {
+		System.out.println("********MENU*********");
+		System.out.println("1. ELIMINAR UNA CANTIDAD ");
+		System.out.println("2. ELIMINAR EL PRODUCTO");
 	}
 }
